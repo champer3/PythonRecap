5dccc1bd941da93ed13289bbe66cad1f5b2396c4
champer3 <okunolas21@gmail.com>
2023-01-06 18:09:44 -0600

    remembering the basics

---
+++ basics.py
@@ -0,0 +1,14 @@
+# String
+book = "labor of the land"
+print(book)
+
+# Function
+def getSum(a, b):
+    return a + b
+def printShit():
+    return "bookie"
+
+figures = [1,2,3,4,5, 6]
+
+for figure in figures:
+    print(figure)
