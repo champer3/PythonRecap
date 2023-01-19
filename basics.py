ab214c11c36de4b75d69eac8a721957691d9a072
champer3 <okunolas21@gmail.com>
2023-01-19 11:24:21 -0600

    Python Syntax Recap

--- basics.py
+++ basics.py
@@ -1,28 +1,29 @@
 # String
 book = "labor of the land"
-print(book)
+print('hi' + 'mom' * 3)
+print(bool(2.0))
+print(int(float('2.0')))
+# # Function
+# def getSum(a, b):
+#     return a + b
+# def printShit():
+#     return "bookie"
 
-# Function
-def getSum(a, b):
-    return a + b
-def printShit():
-    return "bookie"
+# figures = [1,1,1,2,2,2,3,4,5,7,5,2,3,4,5, 6]
 
-figures = [1,1,1,2,2,2,3,4,5,7,5,2,3,4,5, 6]
+# # for figure in figures:
+# #     print(figure)
+
+# count = {}
 
 # for figure in figures:
-#     print(figure)
+#     if figure in count:
+#         count[figure] += 1
+#     else:
+#         count[figure] = 1
 
-count = {}
+# # while count < len(figures):
+# #     print(figures[count])
+# #     count += 1
 
-for figure in figures:
-    if figure in count:
-        count[figure] += 1
-    else:
-        count[figure] = 1
-
-# while count < len(figures):
-#     print(figures[count])
-#     count += 1
-
-print(count)
+# print(count)
